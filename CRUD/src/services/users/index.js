d5d3a46f96948596d3f42c24f34d709e3fe553b0
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-07-02 23:19:46 +0200

    Update index.js

--- CRUD/src/services/users/index.js
+++ CRUD/src/services/users/index.js
@@ -13,54 +13,57 @@ const readFile = (fileName) => {
           return JSON.parse(bufferToString)
 }
 
-
+const usersDB = readFile("users.json")
+const pathJoin = path.join(__dirname, "users.json")
 
 
 // 1. GET all:
 router.get("/", (req, res) =>{
-    const usersDB = readFile("users.json")
     
-    res.send(usersDB)
+    
+    res.status(200).send(usersDB)
+    console.log("The users are:", usersDB)
 })
 
 
 // 2. GET only one
 router.get("/:idu", (req, res) => {
-    const usersDB = readFile("users.json")
-
+    
     const theUser = usersDB.filter( user => user.ID === req.params.idu)
-    res.send(theUser)
+    res.send(usersDB)
+    console.log("The user: ", theUser)
 })
 
 // 3. POST
 router.post("/", (req, res) =>{
-    const usersDB = readFile("users.json")
+    
     const newUser = { ...req.body, ID: uniqid()}
     usersDB.push(newUser)
+    
 
-    fs.writeFileSync(path.join(__dirname, "users.json"), JSON.stringify(usersDB))
-    res.send(usersDB)
+    fs.writeFileSync(pathJoin, JSON.stringify(usersDB))
+    res.status(201).send(usersDB)
 })
 module.exports = router
 
 // 4. PUT 
 router.put("/:idu", (req, res) => {
-    const usersDB = readFile("users.json")
-    const newDB = usersDB.filter((user) => user.ID !== req.params.idu)
+    
+const newDB = usersDB.filter((user) => user.ID !== req.params.idu)
     const modifiedUser = {...req.body, ID: req.params.idu}
     newDB.push(modifiedUser)
-    fs.writeFileSync(path.join(__dirname, "users.json"), JSON.stringify(newDB))
+    fs.writeFileSync(pathJoin, JSON.stringify(newDB))
   
-    res.send(newDB)
+    res.status(201).send(modifiedUser)
   
   })
 
 //5. DELETE
 
 router.delete("/:idu", (req, res) => {
-    const usersDB = readFile("users.json")
-    const filteredUsersArray = usersDB.filter(user => user.ID !== req.params.idu)
-    fs.writeFileSync(path.join(__dirname, "users.json"), JSON.stringify(filteredUsersArray))
+    
+const filteredUsersArray = usersDB.filter(user => user.ID !== req.params.idu)
+    fs.writeFileSync(pathJoin, JSON.stringify(filteredUsersArray))
 
     res.send(filteredUsersArray)
 })
