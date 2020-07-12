8ba00845c613e67dca183e37a57aa8eea8bd599f
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-07-12 20:24:01 +0200

    Update server.js

--- crud-with-errors/src/server.js
+++ crud-with-errors/src/server.js
@@ -1,22 +1,39 @@
 const express = require("express")
-const path = require("path")
+const {join} = require("path")
 const listEndpoints = require("express-list-endpoints")
+//const cors = require("cors")
+
 const productsRouter = require("./services/products")
-const { notFoundHandler, badRequestHandler, genericErrorHandler} = require("./errorHandlers")
+const reviewsRouter = require("./services/rev")
+const routerImg = require("./services/files")
+
+const { notFoundHandler,forbiddenHandler, 
+    unauthorizedHandler, badRequestHandler, genericErrorHandler} = require("./errorHandlers")
 
 const server = express()
 const port = process.env.PORT || 2020
 
+const publicFolderPath = join(__dirname, '../public')
+
 // MidlleWares:
+//server.use(cors())
+server.use(express.static(publicFolderPath))
 server.use(express.json())
 
+
 // Routes:
+
 server.use("/products", productsRouter)
+server.use("/reviews", reviewsRouter)
+server.use("/files", routerImg)
+
 
 // Error HAndlers
 server.use(notFoundHandler)
 server.use(badRequestHandler)
 server.use(genericErrorHandler)
+server.use(forbiddenHandler)
+server.use(unauthorizedHandler)
 
 console.log(listEndpoints(server))
 
